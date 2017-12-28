90eb4db8819a8e288cba6d982be9bb89bae2b3da
Nikolay Radoev <nikolayradoev@gmail.com>
2017-12-28 11:53:24 -0500

    Basic Planet Test

---
+++ PlanetTest/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+
+    public GravityAttractor planet;
+    Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+        rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        // Allow this body to be influenced by planet's gravity
+        planet.Attract(rigidbody);
+    }
+}
---
+++ PlanetTest/Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour {
+
+    public GravityAttractor planet;
+    public Transform PrefabTransform;
+
+	
+	// Update is called once per frame
+	void Update () {
+        //Camera Shake
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SpawnMeteor();
+        }
+    }
+
+    private void SpawnMeteor()
+    {
+        Vector3 spawnPosition = Random.onUnitSphere * 50f;
+        Transform meteor = Instantiate(PrefabTransform, spawnPosition, Quaternion.identity);
+        meteor.GetComponent<GravityBody>().planet = this.planet;
+    }
+}
