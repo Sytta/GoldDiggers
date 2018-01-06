ee112e1e2ff65b5e39c72b33f25ea0a1062be889
NAD.QC.CA\nadjam <Privorotskyd@gmail.com>
2018-01-06 12:31:43 -0500

    Debut du teleport (caleld diablo)

---
+++ MagVsRogues/Assets/MageScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageScript : MonoBehaviour {
+
+    private GameObject diablo = null;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (diablo == null)
+            { 
+                diablo = PhotonNetwork.Instantiate("Diablo", new Vector3(0, 0, 0), Quaternion.identity, 0);
+                diablo.transform.SetParent(this.gameObject.transform);
+                diablo.transform.localPosition = new Vector3(0, 0, 2);
+            }
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Destroy(diablo);
+            diablo = null; 
+        }
+        if (diablo != null)
+        {
+        }
+    }
+}
