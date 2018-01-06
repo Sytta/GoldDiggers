cb009f351157e4ce610b02eaa0c76ab6535ab994
Yawen <sytta@live.ca>
2018-01-06 14:36:57 -0500

    Minimap progressing

--- MagVsRogues/Assets/Scripts/GenericUser.cs
+++ MagVsRogues/Assets/Scripts/GenericUser.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GenericUser : MonoBehaviour {
     public int myID = -1;
-	// Use this for initialization
-	void Start () {
+    private GameManagerCustom gm;
+    [SerializeField] private Transform playerPosition;
+
+    // Use this for initialization
+    void Start () {
         myID = this.gameObject.GetComponent<PhotonView>().ownerId;
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerCustom>();
+
+        if (myID == gm.Round)
+        {
+            playerPosition.GetComponent<Renderer>().material.color = new Color(0f, 1f, 0f);
+        } else
+        {
+            playerPosition.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f);
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
 }
