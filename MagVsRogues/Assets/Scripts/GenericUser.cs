ec786c1e1ef25aeca7ef78beb9640ce66b793155
Aymen Djellal <aymendjellal@gmail.com>
2018-01-06 13:04:41 -0500

    UniqueID present

---
+++ MagVsRogues/Assets/Scripts/GenericUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericUser : MonoBehaviour {
+    public int myID = -1;
+	// Use this for initialization
+	void Start () {
+        myID = this.gameObject.GetComponent<PhotonView>().ownerId;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
