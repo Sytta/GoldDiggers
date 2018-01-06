f8ab676d0c40b0731e0c627b82ce24886ae37da8
yawen <pascalasselin@hotmail.com>
2018-01-06 17:52:35 -0500

    TP workign finally !

--- MagVsRogues/Assets/Scripts/Mage.cs
+++ MagVsRogues/Assets/Scripts/Mage.cs
@@ -9,7 +9,8 @@ public class Mage : MonoBehaviour {
     [SerializeField] private int size = 2;
     // Use this for initialization
     void Start () {
-
+        diablo = Instantiate(Resources.Load("Diablo", typeof(GameObject))) as GameObject;
+        diablo.SetActive(false);    
     }
 
     // Update is called once per frame
@@ -17,9 +18,9 @@ public class Mage : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Z))
         {
 
-            if (diablo == null)
-            { 
-                diablo = PhotonNetwork.Instantiate("Diablo", new Vector3(0, 0, 0), Quaternion.identity, 0);
+            if (!diablo.activeSelf)
+            {
+                diablo.SetActive(true);
                 diablo.transform.SetParent(this.gameObject.transform);
                 diablo.transform.localPosition = new Vector3(0, 0, size);
                 diablo.transform.localScale = new Vector3(size, 0.01f, size);
@@ -28,10 +29,10 @@ public class Mage : MonoBehaviour {
         }
         else if (Input.GetKeyUp(KeyCode.Z))
         {
-            Destroy(diablo);
-            diablo = null; 
+            diablo.GetComponent<TeleporterDiablo>().Jail();
+            diablo.SetActive(false);
         }
-        if (diablo != null)
+        if (diablo.activeSelf)
         {
             diablo.transform.Rotate(0, 4.0f, 0, Space.Self);
         }
