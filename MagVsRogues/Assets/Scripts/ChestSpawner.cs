20139a0fea214ce5ce9c4370131f16a296b94e1a
NAD.QC.CA\nadjam <Privorotskyd@gmail.com>
2018-01-06 11:20:30 -0500

    Random spawner

--- MagVsRogues/Assets/Scripts/ChestSpawner.cs
+++ MagVsRogues/Assets/Scripts/ChestSpawner.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class ChestSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        GameObject newPlayerObject = PhotonNetwork.Instantiate("Chest", this.gameObject.transform.position, Quaternion.identity, 0);
+    [SerializeField] private float GoldMultiplier = 1.0f;
+    [SerializeField] private List<Vector3> Positions;
+    [SerializeField] private List<Vector3> Rotations;
+
+    // Use this for initialization
+    void Start () {
+        int i = Random.Range(0, Positions.Capacity);
+        GameObject newChest = PhotonNetwork.Instantiate("Chest", Positions[i], Quaternion.identity, 0);
+        newChest.gameObject.transform.eulerAngles = Rotations[i];
+        // TODO set ammount of gold !
+        float s = Random.Range(1.0f, GoldMultiplier);
+        newChest.gameObject.GetComponent<ChestController>().gold = (int)(newChest.gameObject.GetComponent<ChestController>().gold * s);
+        //newChest.gameObject.transform.localScale *= s;
+
     }
 
     // Update is called once per frame
---
+++ MagVsRogues/Assets/Scripts/GoldDistribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldDistribute : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] chests = GameObject.FindGameObjectsWithTag("Chest");
+        int totalGold = 0;
+        for(int i = 0; i < chests.Length; ++i)
+        {
+            totalGold += chests[i].GetComponent<ChestController>().gold;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
