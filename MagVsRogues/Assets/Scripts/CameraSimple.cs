cad325653a878c1e6ca6010073131295f4a123a5
Yawen <sytta@live.ca>
2018-01-07 00:05:49 -0500

    Try to see through walls

--- MagVsRogues/Assets/Scripts/CameraSimple.cs
+++ MagVsRogues/Assets/Scripts/CameraSimple.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraSimple : MonoBehaviour
 {
@@ -17,6 +18,14 @@ public class CameraSimple : MonoBehaviour
     float yaw;
     float pitch;
 
+    private Transform prevTransObject;
+    [SerializeField] private Transform camera;
+    [SerializeField] private float alphaValue = 0.1f; // our alpha value
+    [SerializeField] private LayerMask transparentLayers;   // transparency layers.
+    List<Transform> meshes = new List<Transform>();
+
+    private bool stopMoving = false;
+
     void Start()
     {
         if (lockCursor)
@@ -26,9 +35,18 @@ public class CameraSimple : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Target != null)
+        {
+            MakeObjectSemiTransparent();
+        }
+    }
+
     void LateUpdate()
     {
         if (Target == null) return;
+        if (stopMoving) return;
         yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
         pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
@@ -38,6 +56,63 @@ public class CameraSimple : MonoBehaviour
 
         transform.position = Target.position - transform.forward * dstFromTarget;
 
+    }
+
+   void MakeObjectSemiTransparent()
+    {
+        // Cast ray from camera.position to target.position and check if the specified layers are between them.
+        Vector3 direction = (Target.position - camera.position).normalized;
+        RaycastHit[] hits = Physics.RaycastAll(camera.position, direction, Mathf.Infinity);
+        List<Transform> ms = new List<Transform>();
+        foreach (RaycastHit hit in hits)
+        {
+            Transform objectHit = hit.transform;
+            LayerMask layer = objectHit.gameObject.layer;
+
+            if (transparentLayers == (transparentLayers | (1 << layer)))
+            {
+                if (objectHit.GetComponent<Renderer>() != null)
+                {
+                    Debug.Log("Putting " + objectHit + " to transparent");
+                    Material[] mats = objectHit.GetComponent<Renderer>().materials;
+                    ms.Add(objectHit);
+                    foreach (Material mat in mats)
+                    {   
+                        mat.shader = Shader.Find("Hole Shader");
+                    }
+                }
+            }
+        }
+
+        foreach (Transform t in meshes)
+        {
+            if (!ms.Contains(t))
+            {
+                Material[] mats = t.GetComponent<Renderer>().materials;
+                foreach (Material mat in mats)
+                {
+                    mat.shader = Shader.Find("Standard");
+                }
+            }
+        }
+
+        meshes = ms;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            stopMoving = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            stopMoving = false;
+        }
     }
 
 }
